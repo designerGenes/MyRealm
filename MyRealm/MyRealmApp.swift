8ff69e8d2afc14175270925285023e43add267c3
designerjeans <jnationdesignerjeans@gmail.com>
2023-05-24 11:16:25 -0500

    Initial Commit

---
+++ MyRealm/MyRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRealmApp.swift
+//  MyRealm
+//
+//  Created by Jaden Nation on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
